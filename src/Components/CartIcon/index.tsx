be8dd104342071a1c8f825bb67508de0fe460657
BrunoLicuadri <blicuadri@gmail.com>
2023-05-29 14:31:47 -0300

    getting cart items quantity dinamically from localStorage

--- src/Components/CartIcon/index.tsx
+++ src/Components/CartIcon/index.tsx
@@ -1,12 +1,16 @@
 import './styles.css';
 import cartIcon from '../../assets/Images/cart.svg';
+import * as cartService from '../../services/cart-services';
+import { useState } from 'react';
 
 export default function () {
+
+    const [cart, setCart] = useState(cartService.getCart());
 
     return (
         <>
             <img src={cartIcon} alt="Carrinho de Compras" />
-            <div className="gkc-cart-count">22</div>
+            <div className="gkc-cart-count">{cart.items.length}</div>
         </>
 
     )
