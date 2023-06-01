b2751e78b4481dd91191008230e45c7c33e51003
BrunoLicuadri <blicuadri@gmail.com>
2023-06-01 11:15:01 -0300

    login form and login request

--- src/Routes/ClientHome/Login/index.tsx
+++ src/Routes/ClientHome/Login/index.tsx
@@ -1,20 +1,60 @@
 import './styles.css';
+import { useState } from 'react';
+import { CredentialsDTO } from '../../../models/auth';
+import * as authService from '../../../services/auth-services';
 
 export default function Login() {
+
+    const [formData, setFormData] = useState<CredentialsDTO>({
+        username: '',
+        password: ''
+    })
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+        authService.loginRequest(formData)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log("Erro no Login", error);
+            })
+    }
+
+    function handleInputChange(event: any) {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFormData({ ...formData, [name]: value });
+    }
 
     return (
         <main>
             <section id="login-section" className="gkc-container">
                 <div className="gkc-login-form-container">
-                    <form className="gkc-card gkc-form">
+                    <form onSubmit={handleSubmit} className="gkc-card gkc-form">
                         <h2>Login</h2>
                         <div className="gkc-form-controls-container">
                             <div>
-                                <input className="gkc-form-control" type="text" placeholder="Email" />
-                                    <div className="gkc-form-error"></div>
+                                <input
+                                    name="username"
+                                    value={formData.username}
+                                    className="gkc-form-control"
+                                    type="text"
+                                    placeholder="Email"
+                                    onChange={handleInputChange}
+                                />
+                                <div className="gkc-form-error"></div>
                             </div>
                             <div>
-                                <input className="gkc-form-control" type="password" placeholder="Senha" />
+                                <input
+                                    name="password"
+                                    value={formData.password}
+                                    className="gkc-form-control"
+                                    type="password"
+                                    placeholder="Senha"
+                                    onChange={handleInputChange}
+                                />
                             </div>
                         </div>
 
