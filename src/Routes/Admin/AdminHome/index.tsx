e58e6f5363ac193aa02f70cbf07c89e259b3766a
BrunoLicuadri <blicuadri@gmail.com>
2023-06-02 11:06:25 -0300

    Getting user

--- src/Routes/Admin/AdminHome/index.tsx
+++ src/Routes/Admin/AdminHome/index.tsx
@@ -1,11 +1,27 @@
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { UserDTO } from '../../../models/user';
+import * as userService from '../../../services/user-service';
 
 export default function AdminHome() {
+
+    const [user, setUser] = useState<UserDTO>();
+
+    useEffect( ()=>{
+        userService.findMe()
+        .then( response=>{
+            setUser(response.data);
+            console.log(response);
+        })
+        .catch( error=>{
+            console.log("Error capturing name", error);
+        })
+    }, []);
 
     return (
         <main>
             <section id="admin-home-section" className="gkc-container">
-                <h2 className="gkc-section-title gkc-mb20">Bem-vindo à área administrativa</h2>
+                <h2 className="gkc-section-title gkc-mb20">Bem-vindo à área administrativa {user?.name}</h2>
             </section>
         </main>
 
--- src/services/auth-services.ts
+++ src/services/auth-services.ts
@@ -33,6 +33,6 @@ export function saveAccessToken(token: string) {
 }
 
 export function getAccessToken() {
-    accessTokenRepository.get();
+    return accessTokenRepository.get();
 }
 
