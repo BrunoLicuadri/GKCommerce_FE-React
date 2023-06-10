1ce4e9a9ff26baab95feccae8628cf86dbbba364
BrunoLicuadri <blicuadri@gmail.com>
2023-06-10 13:41:57 -0300

    Access control routes level

--- src/Components/PrivateRoute/index.tsx
+++ src/Components/PrivateRoute/index.tsx
@@ -1,14 +1,19 @@
 import { Navigate } from "react-router-dom";
 import * as authService from "../../services/auth-services";
+import { RoleEnum } from "../../models/auth";
 
 type Props = {
     children: JSX.Element;
+    roles?: RoleEnum[]
 }
 
-export default function PrivateRoute({ children }: Props) {
+export default function PrivateRoute({ children, roles = [] }: Props) {
 
     if (!authService.isAuthenticated()) {
         return <Navigate to="/login" />;
+    }
+    if (!authService.hasAnyRoles(roles)) {
+        return <Navigate to="/catalog" />;
     }
     return children
 
