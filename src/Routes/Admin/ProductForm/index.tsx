e05673044299b9bb7e05c75badcf27942297ba6b
BrunoLicuadri <blicuadri@gmail.com>
2023-06-14 11:09:20 -0300

    layout product listing & product CRUD static

--- src/Routes/Admin/ProductForm/index.tsx
+++ src/Routes/Admin/ProductForm/index.tsx
@@ -1,7 +1,42 @@
+import './styles.css';
 
-export default function ProductForm(){
+export default function ProductForm() {
 
-    return(
-        <h1>Product Form</h1>
+    return (
+        <main>
+            <section id="product-form-section" className="gkc-container">
+                <div className="gkc-product-form-container">
+                    <form className="gkc-card gkc-form">
+                        <h2>Dados do produto</h2>
+                        <div className="gkc-form-controls-container">
+                            <div>
+                                <input className="gkc-form-control" type="text" placeholder="Nome"/>
+                            </div>
+                            <div>
+                                <input className="gkc-form-control" type="text" placeholder="Preço"/>
+                            </div>
+                            <div>
+                                <input className="gkc-form-control" type="text" placeholder="Imagem"/>
+                            </div>
+                            <div>
+                                <select className="gkc-form-control gkc-select" required>
+                                    <option value="" disabled selected>Categorias</option>
+                                    <option value="1">Valor 1</option>
+                                    <option value="2">Valor 2</option>
+                                </select>
+                            </div>
+                            <div>
+                                <textarea className="gkc-form-control gkc-textarea" placeholder="Descrição"></textarea>
+                            </div>
+                        </div>
+
+                        <div className="gkc-product-form-buttons">
+                            <button type="reset" className="gkc-btn gkc-btn-white">Cancelar</button>
+                            <button type="submit" className="gkc-btn gkc-btn-blue">Salvar</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </main>
     );
 }
--- src/Routes/Admin/ProductListing/index.tsx
+++ src/Routes/Admin/ProductListing/index.tsx
@@ -1,7 +1,64 @@
+import './styles.css';
+import editIcon from '../../../assets/Images/edit.svg';
+import deleteIcon from '../../../assets/Images/delete.svg';
+import computer from '../../../assets/Images/computer.png';
 
-export default function ProductListing(){
+export default function ProductListing() {
 
-    return(
-        <h1>Product Listing</h1>
+    return (
+        <main>
+            <section id="product-listing-section" className="gkc-container">
+                <h2 className="gkc-section-title gkc-mb20">Cadastro de produtos</h2>
+
+                <div className="gkc-btn-page-container gkc-mb20">
+                    <div className="gkc-btn gkc-btn-white">Novo</div>
+                </div>
+
+                <form className="gkc-search-bar">
+                    <button type="submit">🔎︎</button>
+                    <input type="text" placeholder="Nome do produto" />
+                    <button type="reset">🗙</button>
+                </form>
+
+                <table className="gkc-table gkc-mb20 gkc-mt20">
+                    <thead>
+                        <tr>
+                            <th className="gkc-tb576">ID</th>
+                            <th></th>
+                            <th className="gkc-tb768">Preço</th>
+                            <th className="gkc-txt-left">Nome</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="gkc-tb576">341</td>
+                            <td><img className="gkc-product-listing-image" src={computer} alt="Computer" /></td>
+                            <td className="gkc-tb768">R$ 5000,00</td>
+                            <td className="gkc-txt-left">Computador Gamer XT Plus Ultra</td>
+                            <td><img className="gkc-product-listing-btn" src={editIcon} alt="Editar" /></td>
+                            <td><img className="gkc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
+                        </tr>
+                        <tr>
+                            <td className="gkc-tb576">341</td>
+                            <td><img className="gkc-product-listing-image" src={computer} alt="Computer" /></td>
+                            <td className="gkc-tb768">R$ 5000,00</td>
+                            <td className="gkc-txt-left">Computador Gamer XT Plus Ultra</td>
+                            <td><img className="gkc-product-listing-btn" src={editIcon} alt="Editar" /></td>
+                            <td><img className="gkc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>                      </tr>
+                        <tr>
+                            <td className="gkc-tb576">341</td>
+                            <td><img className="gkc-product-listing-image" src={computer} alt="Computer" /></td>
+                            <td className="gkc-tb768">R$ 5000,00</td>
+                            <td className="gkc-txt-left">Computador Gamer XT Plus Ultra</td>
+                            <td><img className="gkc-product-listing-btn" src={editIcon} alt="Editar" /></td>
+                            <td><img className="gkc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>                     </tr>
+                    </tbody>
+                </table>
+
+                <div className="gkc-btn-next-page">Carregar mais</div>
+            </section>
+        </main>
     );
 }
