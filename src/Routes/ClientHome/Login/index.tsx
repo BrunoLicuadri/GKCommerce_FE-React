b1a225fc44e33f0355a6f9114d130136e49d5f78
BrunoLicuadri <blicuadri@gmail.com>
2023-06-18 13:47:06 -0300

    Custom FormInput

--- src/Routes/ClientHome/Login/index.tsx
+++ src/Routes/ClientHome/Login/index.tsx
@@ -1,24 +1,40 @@
 import './styles.css';
 import { useContext, useState } from 'react';
-import { CredentialsDTO } from '../../../models/auth';
-import * as authService from '../../../services/auth-services';
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
+import * as authService from '../../../services/auth-services';
+import FormInput from '../../../Components/FormInput';
 
 export default function Login() {
 
     const navigate = useNavigate();
 
-    const {setContextTokenPayload} = useContext(ContextToken);
+    const { setContextTokenPayload } = useContext(ContextToken);
 
-    const [formData, setFormData] = useState<CredentialsDTO>({
-        username: '',
-        password: ''
+    const [formData, setFormData] = useState<any>({
+        username: {
+            value: "",
+            id: "username",
+            name: "username",
+            type: "text",
+            placeholder: "Email",
+            validation: function (value: string) {
+                return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value.toLowerCase());
+            },
+            message: "Favor informar um email válido",
+        },
+        password: {
+            value: "",
+            id: "password",
+            name: "password",
+            type: "password",
+            placeholder: "Senha",
+        }
     })
 
     function handleSubmit(event: any) {
         event.preventDefault();
-        authService.loginRequest(formData)
+        authService.loginRequest({ username: formData.username.value, password: formData.password.value })
             .then(response => {
                 authService.saveAccessToken(response.data.access_token);
                 setContextTokenPayload(authService.getAccessTokenPayload());
@@ -33,7 +49,7 @@ export default function Login() {
         const name = event.target.name;
         const value = event.target.value;
 
-        setFormData({ ...formData, [name]: value });
+        setFormData({ ...formData, [name]: { ...formData[name], value: value } });
     }
 
     return (
@@ -44,23 +60,17 @@ export default function Login() {
                         <h2>Login</h2>
                         <div className="gkc-form-controls-container">
                             <div>
-                                <input
-                                    name="username"
-                                    value={formData.username}
+                                <FormInput
+                                    {...formData.username}
                                     className="gkc-form-control"
-                                    type="text"
-                                    placeholder="Email"
                                     onChange={handleInputChange}
                                 />
                                 <div className="gkc-form-error"></div>
                             </div>
                             <div>
-                                <input
-                                    name="password"
-                                    value={formData.password}
+                                <FormInput
+                                    {...formData.password}
                                     className="gkc-form-control"
-                                    type="password"
-                                    placeholder="Senha"
                                     onChange={handleInputChange}
                                 />
                             </div>
